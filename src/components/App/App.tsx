ffbfc863b6350f0429f19459670669d01e7d95f0
smelik81 <artem.smelyanskiy81@gmail.com>
2025-12-15 01:57:39 +0200

    modified folder components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import css from "./App.module.css";
 import { useState } from "react";
-import CafeInfo from "./components/CafeInfo/CafeInfo";
-import Notification from "./components/Notification/Notification";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
-import type { VoteType, Votes } from "./types/votes";
-import VoteStats from "./components/VoteStats/VoteStats";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import Notification from "../Notification/Notification";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import type { VoteType, Votes } from "../../types/votes";
+import VoteStats from "../VoteStats/VoteStats";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
